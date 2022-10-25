5925ae5187f60bf0f2fab2db78d51756f86d0e1c
brunomoreirab <brunomoreirabarbosa@gmail.com>
2022-10-25 11:30:11 -0300

    Projeto concluído

--- index.js
+++ index.js
@@ -24,9 +24,48 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", (req, res) => {
+  // Busca a hora atual
+  let input_date_format = new Date()
 
+  response_obj = {
+    unix: input_date_format.valueOf(), 
+    utc: input_date_format.toGMTString()}
+
+  // Retorna o objeto
+  res.json(response_obj)
+})
+
+app.get("/api/:date", (req, res) => {
+  let input = req.params.date;
+  let response_obj = {}
+  console.log(input)
+  // input.match(/^\d{4}-\d{2}-\d{2}$/)
+  // Verifica se é apenas número (UNIX) para converter a string para number
+  if(input.match(/^-?\d+$/)){
+    input = +input;
+  }
+
+  // Converte para formato de data
+  let input_date_format = new Date(input);
+
+  // Verifica se o formato é válido ou não, e constrói o objeto de resposta
+  if (input_date_format == 'Invalid Date') {
+    response_obj = {
+      error: "Invalid Date"}
+  }
+
+  else {
+    response_obj = {
+      unix: input_date_format.valueOf(), 
+      utc: input_date_format.toGMTString()}
+  }
+
+  // Retorna o objeto
+  res.json(response_obj)
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
